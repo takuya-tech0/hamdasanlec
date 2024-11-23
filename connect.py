1efadc4f0f0e1b62e7c96ac324d4c8d7310bcbd1
takuya-tech0 <140943306+takuya-tech0@users.noreply.github.com>
2024-11-23 21:02:21 +0900

    Update connect.py

--- connect.py
+++ connect.py
@@ -2,27 +2,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 # データベース接続設定
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'password',
-    'database': 'test_system'
+config = {
+    'host': 'tech0-db-step4-studentrdb-3.mysql.database.azure.com',
+    'user': 'tech0gen7student',
+    'password': '',
+    'database': 'sembaapp',
+    'ssl_ca': 'DigiCertGlobalRootCA.crt.pem'
 }
 
 def test_mysql_connection():
     try:
-        # 接続文字列を設定から構築
-        connection_string = (
-            f"mysql+pymysql://{db_config['user']}:{db_config['password']}"
-            f"@{db_config['host']}/{db_config['database']}"
-        )
+        # 接続文字列を作成
+        connection_string = f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}/{config['database']}"
         
-        # エンジンを作成
-        engine = create_engine(connection_string)
+        # エンジンを作成（SSL設定付き）
+        engine = create_engine(connection_string, connect_args={'ssl_ca': config['ssl_ca']})
         
         # 接続テスト
         with engine.connect() as connection:
-            print("MySQLへの接続に成功しました！")
+            print("データベースへの接続に成功しました！")
             
     except SQLAlchemyError as e:
         print(f"エラーが発生しました: {str(e)}")
